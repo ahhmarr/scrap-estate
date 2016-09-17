cff3142b3ccb3b64a45e14a626468ced4a10025c
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2016-09-17 15:48:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var casper=require('casper').create({
+	verbose : true/*,
+	logLevel : 'debug'*/
+});
+
+var options=casper.cli.options;
+var loc=options.loc || '';
+var beds=options.beds || '';
+var minPrice=options.minPrice || '';
+var maxPrice=options.maxPrice || '';
+var page=options.page || 1;
+var type=options.type || 2; //rent or buy 1 buy 2 rent
+var url='https://www.propertyfinder.ae/search?l='+loc+'&q=&c='+type+'&t=&rp=y&pf='+minPrice+'&pt='+maxPrice+'&bf='+beds+'&page='+page+'&ob=pa' //ob=pd for price des;
+var properties=[];
+console.log(url);
+
+casper.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36');
+
+function getLinks()
+{
+	function clean(text){
+		return text.replace("\n","").trim();
+	}
+	var elms=[];
+
+	$('.serp-result > li').each(function(index,elm)
+	{
+		var elm=$(elm);
+		var name=elm.find('bdi').html();
+		var img=elm.find('img').attr('src');
+		var beds=elm.find('.property-details span:nth-child(2)').text();
+		var area=elm.find('.property-details span:nth-child(4)').text().replace("\n","").trim();
+		if(!name)
+			return;
+		elms.push({
+			name : elm.find('bdi').html(),
+			value : elm.find('span.val').html(),
+			currency : elm.find('span.currency').html(),
+			period : elm.find('span.period').html(),
+			address : elm.find('.location-tree').text().replace("\n","").trim(),
+			beds : beds.replace("\n","").trim(),
+			img : img,
+			area : area
+		});
+	});
+	return elms;
+}
+casper.start(url,function(){
+	properties=this.evaluate(getLinks)
+});
+casper.run(function()
+{
+	this.echo(JSON.stringify(properties));
+	casper.done();
+});
---
+++ server.js
@@ -0,0 +1,48 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var shellescape = require('shell-escape');
+
+app.use(bodyParser.json());
+app.get('/', function (req, res) {
+    var loc=req.query.loc,
+        beds=req.query.beds,
+        maxPrice=req.query.max,
+        minPrice=req.query.min,
+        type=req.query.type,
+        page=req.query.page || 1,
+        domain=(req.query.domain || 'propertyfinder')+'.js';
+
+    var args = [
+        'casperjs',
+        domain
+    ];
+   if(loc)
+    args.push('--loc="'+loc+'"');
+   if(beds)
+    args.push('--beds="'+beds+'"');
+  if(minPrice)
+    args.push('--minPrice="'+minPrice+'"');
+  if(maxPrice)
+    args.push('--maxPrice="'+maxPrice+'"');
+  if(type)
+    args.push('--type="'+type+'"');
+  if(type)
+    args.push('--page="'+page+'"');
+
+    var cmd = args.join(" ");
+
+    exec(cmd, function (error, stdout) {
+        if(error) console.log(error);
+        res.end(stdout);
+    });
+
+});
+
+var server = app.listen(3001, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
